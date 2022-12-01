2b75646da6ade733402a9179cb2fea5920fd6915
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-12-01 01:13:53 -0600

    Initial Commit

---
+++ Sophos App/Sophos_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sophos_AppApp.swift
+//  Sophos App
+//
+//  Created by 1801C724748LM on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sophos_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
