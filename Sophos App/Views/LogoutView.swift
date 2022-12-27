c735131013333cd796d6630311e9241b51d299bc
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-12-27 12:16:55 -0500

    checking that views are displayed

--- Sophos App/Views/LogoutView.swift
+++ Sophos App/Views/LogoutView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LogoutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, let's logout!")
     }
 }
 
--- Sophos App/Views/SendDocumentsView.swift
+++ Sophos App/Views/SendDocumentsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SendDocumentsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, ready to send your docs?")
     }
 }
 
