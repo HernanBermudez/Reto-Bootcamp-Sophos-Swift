1b464da0016d2793a5c0c3399c6418db0ebf99b0
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2023-01-04 22:38:49 -0500

    ViewModel for SeeDocumentsView

---
+++ Sophos App/ViewModels/SeeDocumentsViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  SeeDocumentsViewModel.swift
+//  Sophos App
+//
+//  Created by 1801C724748LM on 04/01/2023.
+//
+
+import Foundation
+
+class SeeDocumentsViewModel : ObservableObject {
+    @Published var selectedDocument : String = ""
+    @Published var fetchedDocuments = FetchDocumentsModel(Items: [FetchDocumentsItem()])
+    @Published var documentList : SeeDocumentsList = SeeDocumentsList()
+    @Published var formatterDate = DateFormatter()
+    @Published var datesArray : DateArray = DateArray()
+    //@Published var fetchedDocuments = OfficesItems(Items: [Offices()])
+    
+    func decodeImage(base64 : String?){
+        let rebornImg = base64?.imageFromBase64
+        //let rebornImg = base64?.imageFromBase64
+        
+    }
+    
+    func dateFormatter(date : String) -> String {
+        var dateSplitted = date
+        let ix = dateSplitted.startIndex // the index of 1st character
+        let ix2 = dateSplitted.index(ix, offsetBy: 10) // the index of 10th character
+        let ix3 = dateSplitted.index(ix, offsetBy: dateSplitted.count - 1) // the index of last character
+
+        dateSplitted.removeSubrange(ix2...ix3)
+        var dateFormatted = dateSplitted.split(separator: "-")
+        let finalDate = String(dateFormatted[2] + "-" + dateFormatted[1] + "-" + dateFormatted[0])
+        return finalDate
+    }
+
+    func fetchDocuments(email : String) async {
+        guard let url = URL(string: documentsUrl + "?correo=" + email.lowercased()) else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data,_) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try! JSONDecoder().decode(FetchDocumentsModel?.self, from: data){
+                fetchedDocuments = decodedResponse
+                documentList.documents = decodedResponse.Items
+            }
+            for dates in documentList.documents {
+                datesArray.dates.append(dateFormatter(date: dates.Fecha))
+            }
+        } catch {
+            print("Failed retrieving documents")
+        }
+        //jsonString = String(data: encodedDocument, encoding: .utf8)!
+        //print(jsonString)
+    }
+}
