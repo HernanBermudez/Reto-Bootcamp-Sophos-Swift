75a536ac8fc8adfd4bbded07764c6769a39b5a63
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2023-01-05 16:58:24 -0500

    sheet for image of adjunto from API

---
+++ Sophos App/Views/SeeDocumentsImageView.swift
@@ -0,0 +1,25 @@
+//
+//  SeeDocumentsImageView.swift
+//  Sophos App
+//
+//  Created by 1801C724748LM on 05/01/2023.
+//
+
+import SwiftUI
+
+struct SeeDocumentsImageView: View {
+    @EnvironmentObject var seeDocumentsVM : SeeDocumentsViewModel
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack{
+            Image(systemName: "person.circle.fill")
+            Button("Press to dismiss") {
+                dismiss()
+            }
+            .font(.title)
+            .padding()
+            .background(.black)
+        }
+    }
+}
