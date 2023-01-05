ed57f4cb12715b9bc0aa8449a5766ed8d81edd80
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2023-01-04 22:39:45 -0500

    initial see documents view

--- Sophos App/Views/SeeDocumentsView.swift
+++ Sophos App/Views/SeeDocumentsView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct SeeDocumentsView: View {
+    @ObservedObject var seeDocumentsVM = SeeDocumentsViewModel()
+    @EnvironmentObject var loginVM : LoginViewModel
+ 
     var body: some View {
         NavigationSplitView {
             Text("Hello, check out your docs!")
+            Spacer()
+            List {
+                ForEach(seeDocumentsVM.documentList.documents, id: \.self){ doc in
+                    GroupBox{
+                        Text(doc.Nombre + doc.Apellido)
+                        Text(doc.TipoAdjunto)
+                    }
+                }
+                ForEach(seeDocumentsVM.datesArray.dates, id: \.self){date in
+                    Text(date)
+                }
+            }
         } detail: {
             
         }.toolbar(content: {
             NavMenu()
         })
+        .onAppear{
+            Task{
+                await seeDocumentsVM.fetchDocuments(email: loginVM.email)
+            }
+        }
+        .environmentObject(loginVM)
         .navigationTitle("Documentos")
 
         
