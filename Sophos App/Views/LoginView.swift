ac93db23616a35e4dcd1305ee3da4a14a19148ef
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2023-01-09 01:01:38 -0500

    biometrics auth button set

--- Sophos App/Views/LoginView.swift
+++ Sophos App/Views/LoginView.swift
@@ -24,12 +24,11 @@ struct LoginView: View {
                     .font(.title3).foregroundColor(colorSophos)
                     .fontWeight(.bold)
                     .multilineTextAlignment(.center)
-                //.offset(y:-70)
                 
                 HStack{
                     Image(systemName: "person.circle.fill").padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 0))
                     TextField("Email", text: $loginVM.email)
-                }.background(RoundedRectangleInput()).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))//.offset(y:-60)
+                }.background(RoundedRectangleInput()).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
                 
                 HStack{
                     Image(systemName: "key.fill").padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 0))
@@ -37,7 +36,7 @@ struct LoginView: View {
                     Image(systemName: "eye.fill").padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)).onTapGesture {
                         
                     }
-                }.background(RoundedRectangleInput()).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))//.offset(y:-60)
+                }.background(RoundedRectangleInput()).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
                 
                 Text(loginVM.loginErrors) .font(.title3).foregroundColor(Color.red)
                     .fontWeight(.bold)
@@ -62,21 +61,17 @@ struct LoginView: View {
                     NavigationLink(loginVM.isLoggedIn || loginVM.inProgress ? "" : "Ingresar", value: true)
                 }
                 
-                HStack{
-                    Image(systemName: "touchid")
-                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                    Button(action: {}){
-                        Text("Ingresar con huella")
-                            .bold()
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .foregroundColor(colorSophos)
-                            .background(Color.white)
-                            .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 50))
+                AsyncButton(isComplete: loginVM.isUnlocked, action: {
+                    Task{
+                        await loginVM.authenticate(email: loginVM.email, password: loginVM.password)
                     }
-                }.background(RoundedRectangle(cornerRadius: 25)
-                    .stroke(colorSophos, lineWidth: 2))
-                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
-                //.offset(y:-20)
+                }) {
+                    HStack{
+                        Image(systemName: "touchid")
+                            .padding(EdgeInsets(top: 0, leading: -20, bottom: 0, trailing: 0))
+                        Text("Ingresar con huella")
+                    }
+                }
             }.padding()
                 .navigationDestination(isPresented: $loginVM.isPresented, destination: {
                     NavigationStack{
